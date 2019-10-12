9fd01c7c16d1ef76b86ce0ca42f2e159a469aee9
mytvpro <hosting@tvstreams.xyz>
2019-10-12 03:45:23 -0400

    attempt to fix separate stream values per server

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -327,25 +327,37 @@ include "header.php";
                             </div> <!-- end col -->
                             
                             <div class="col-md-6 col-xl-3">
-                                <div class="card-box total-active-streams">
+                                <div class="card-box">
                                     <div class="row">
                                         <div class="col-6">
                                             <div class="text-right">
-                                                <h3 class="text-dark my-1"><span data-plugin="counterup" class="entry">0</span></h3>
+                                                <h3 class="text-dark my-1 active-live-streams"><span data-plugin="counterup" class="entry">0</span></h3>
                                                 <p class="text-muted mb-1 text-truncate"><a href="./streams.php">Active Live Streams</a></p>
                                             </div>
                                             <div class="text-right">
-                                                <h3 class="text-dark my-1"><span data-plugin="counterup" class="entry">0</span></h3>
+                                                <h3 class="text-dark my-1 active-movie-streams"><span data-plugin="counterup" class="entry">0</span></h3>
                                                 <p class="text-muted mb-1 text-truncate"><a href="./streams.php">Active Movie Streams</a></p>
                                             </div>
                                             <div class="text-right">
-                                                <h3 class="text-dark my-1"><span data-plugin="counterup" class="entry">0</span></h3>
+                                                <h3 class="text-dark my-1 active-series-streams"><span data-plugin="counterup" class="entry">0</span></h3>
                                                 <p class="text-muted mb-1 text-truncate"><a href="./streams.php">Active Series Streams</a></p>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="mt-3">
-                                        <h6 class="text-uppercase"><a href="./streams/php">Total Active Streams</a> <span class="float-right entry-percentage">0</span></h6>
+                                        <h6 class="text-uppercase total-active-streams"><a href="./streams/php">Total Active Streams</a> <span class="float-right entry-percentage">0</span></h6>
+                                        <div class="progress progress-sm m-0">
+                                            <div class="progress-bar bg-purple" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
+                                                <span class="sr-only">0%</span>
+                                            </div>
+                                        </div>
+                                        <h6 class="text-uppercase total-live-streams"><a href="./streams/php">Total Live Streams</a> <span class="float-right entry-percentage">0</span></h6>
+                                        <div class="progress progress-sm m-0">
+                                            <div class="progress-bar bg-purple" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
+                                                <span class="sr-only">0%</span>
+                                            </div>
+                                        </div>
+                                        <h6 class="text-uppercase total-offline-streams"><a href="./streams/php">Total Offline Streams</a> <span class="float-right entry-percentage">0</span></h6>
                                         <div class="progress progress-sm m-0">
                                             <div class="progress-bar bg-purple" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
                                                 <span class="sr-only">0%</span>
