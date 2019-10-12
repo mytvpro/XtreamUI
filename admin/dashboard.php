c166ed49da7489e0149f96bbfc15e012aed66dfa
mytvpro <hosting@tvstreams.xyz>
2019-10-12 03:30:46 -0400

    fixed parent divs

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -321,24 +321,24 @@ include "header.php";
                             </div> <!-- end col -->
                             
                             <div class="col-md-6 col-xl-3">
-                                <div class="">
+                                <div class="card-box total-active-streams">
                                     <div class="row">
                                         <div class="col-6">
-                                            <div class="text-right card-box active-live-streams">
+                                            <div class="text-right">
                                                 <h3 class="text-dark my-1"><span data-plugin="counterup" class="entry">0</span></h3>
                                                 <p class="text-muted mb-1 text-truncate"><a href="./streams.php">Active Live Streams</a></p>
                                             </div>
-                                            <div class="text-right card-box active-movie-streams">
+                                            <div class="text-right">
                                                 <h3 class="text-dark my-1"><span data-plugin="counterup" class="entry">0</span></h3>
                                                 <p class="text-muted mb-1 text-truncate"><a href="./streams.php">Active Movie Streams</a></p>
                                             </div>
-                                            <div class="text-right card-box active-series-streams">
+                                            <div class="text-right">
                                                 <h3 class="text-dark my-1"><span data-plugin="counterup" class="entry">0</span></h3>
                                                 <p class="text-muted mb-1 text-truncate"><a href="./streams.php">Active Series Streams</a></p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="mt-3 card-box total-active-streams">
+                                    <div class="mt-3">
                                         <h6 class="text-uppercase"><a href="./streams/php">Total Active Streams</a> <span class="float-right entry-percentage">0</span></h6>
                                         <div class="progress progress-sm m-0">
                                             <div class="progress-bar bg-purple" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
